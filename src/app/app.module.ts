52442fe904da8a8dbec2ea6e1f82cad89f6b2948
Miss Ahrizona <missahrizona@outlook.com>
2022-08-08 18:43:28 -0500

    removed imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,12 @@ import { AppComponent } from './app.component';
 import { ErrorComponent } from './error/error.component';
 import { IonicModule } from '@ionic/angular';
 import { AppAssistant } from './portfolio/child-classes/AppAssistant';
-import { FeaturelistComponent } from './portfolio/child-classes/featurelist/featurelist.component';
-import { TimelineComponent } from './portfolio/child-classes/timeline/timeline.component';
 import { LoginComponent } from './login/login.component';
 import { TelCardComponent } from './login/child-classes/tel-card/tel-card.component';
 import { DisplaynameCardComponent } from './login/child-classes/displayname-card/displayname-card.component';
 import { SmscodeCardComponent } from './login/child-classes/smscode-card/smscode-card.component';
 import { GreetingCardComponent } from './login/child-classes/greeting-card/greeting-card.component';
 import { LogoComponent } from './logo/logo.component';
-import { ToggleLink } from './portfolio/child-classes/togglelink';
 import { SplashscreenComponent } from './splashscreen/splashscreen.component';
 import { LazyImgDirective } from './directives/lazy-img.directive';
 
