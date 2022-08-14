f8326f82fa38bad9e4ddb9f8af4017d625a97211
Miss Ahrizona <missahrizona@outlook.com>
2022-08-14 07:07:25 -0500

    env vars

--- api/_mongoclient.ts
+++ api/_mongoclient.ts
@@ -1,11 +1,8 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 console.log(process.env.MONGO_DB_CONNECTION_STRING);
-const client = new MongoClient(
-  'mongodb+srv://princesslily:chokemedaddy69@charmee.vp0wy.mongodb.net/?retryWrites=true&w=majority',
-  {
-    serverApi: ServerApiVersion.v1,
-  }
-);
+const client = new MongoClient(process.env.MONGO_DB_CONNECTION_STRING, {
+  serverApi: ServerApiVersion.v1,
+});
 
 (async () => {
   await client.connect();
