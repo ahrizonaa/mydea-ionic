e11412a5733e116f171feb946d81f49de96190e0
Miss Ahrizona <missahrizona@outlook.com>
2022-08-15 11:44:54 -0500

    testing /user/fetch

--- api/user/fetch.ts
+++ api/user/fetch.ts
@@ -5,6 +5,9 @@ import { cors } from 'api/lib/_cors';
 
 export default cors(async function (req: VercelRequest, res: VercelResponse) {
   try {
+    if (req.method == 'GET') {
+      res.status(200).send('User fetch works!');
+    }
     let result = await db
       .collection('Users')
       .findOne({ _id: new ObjectId(req.body._id) });
