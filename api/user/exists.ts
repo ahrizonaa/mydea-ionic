8d8c7982debc9d983fa4e13111c8436a1b6893c9
missahrizona <princessssahri@gmail.com>
2022-08-19 04:00:23 -0500

    testing exists

--- api/user/exists.ts
+++ api/user/exists.ts
@@ -3,8 +3,6 @@ import { db } from '../lib/_db';
 
 export default async function (req: VercelRequest, res: VercelResponse) {
   try {
-    res.status(200).send('auth/exists received');
-    return;
     let search = await db.collection('Users').findOne({ tel: req.body.tel });
     res.status(200).send(search);
   } catch (exception) {
