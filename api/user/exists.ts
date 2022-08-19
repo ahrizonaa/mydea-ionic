f58ffa93480deca367375bea7202af10aa22773e
missahrizona <princessssahri@gmail.com>
2022-08-19 02:38:01 -0500

    custom error msg

--- api/user/exists.ts
+++ api/user/exists.ts
@@ -1,5 +1,4 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { ObjectId } from 'mongodb';
 import { db } from '../lib/_db';
 
 export default async function (req: VercelRequest, res: VercelResponse) {
@@ -7,6 +6,9 @@ export default async function (req: VercelRequest, res: VercelResponse) {
     let search = await db.collection('Users').findOne({ tel: req.body.tel });
     res.status(200).send(search);
   } catch (exception) {
-    res.status(500).send(exception);
+    res.status(500).send({
+      msg: 'Error searching for user',
+      exception: exception,
+    });
   }
 }
