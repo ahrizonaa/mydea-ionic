32e0dc19337dcd9a5680c76b36b66eeee055cdc3
missahrizona <princessssahri@gmail.com>
2022-09-12 03:20:38 -0500

    fixed bg swiper

--- src/app/components/poster-slider/poster-slider.component.ts
+++ src/app/components/poster-slider/poster-slider.component.ts
@@ -1,40 +1,20 @@
-import {
-  Component,
-  ViewChild,
-  AfterViewInit,
-  ElementRef,
-  Input,
-} from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Component, Input } from '@angular/core';
 
-import Swiper, { EffectCreative, Parallax } from 'swiper';
+import SwiperCore, { EffectCards } from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([EffectCards]);
 
 @Component({
   selector: 'app-poster-slider',
   templateUrl: './poster-slider.component.html',
   styleUrls: ['./poster-slider.component.css'],
 })
-export class PosterSliderComponent implements AfterViewInit {
+export class PosterSliderComponent {
   @Input() cards: any[];
-  @ViewChild('swiper') swiper: any;
 
-  creativeEffect: any;
-  constructor() {
-    this.creativeEffect = {
-      limitProgress: 3,
-      perspective: true,
-      shadowPerProgress: true,
-      prev: {
-        shadow: true,
-        translate: ['-15%', 0, -200],
-      },
-      next: {
-        translate: [this.calcNextOffset(), 0, 0],
-      },
-    };
-  }
-  ngAfterViewInit() {
-    this.swiper.modules = [Parallax, EffectCreative];
-  }
+  constructor(public auth: AuthService) {}
 
   formatBgImgCss(i: number): string {
     const str = `url('../../assets/images/backgrounds/bg-${i + 1}.jpg')`;
@@ -43,15 +23,5 @@ export class PosterSliderComponent implements AfterViewInit {
 
   getImgUrl(i: number): string {
     return `../../assets/images/backgrounds/bg-${i + 1}.jpg`;
-  }
-
-  calcNextOffset(): string {
-    if (this.swiper == undefined) return;
-    const parentWidth = this.swiper.nativeElement.parentElement.offsetWidth;
-    const swiperWidth = this.swiper.nativeElement.offsetWidth;
-    let nextOffset =
-      (parentWidth - (parentWidth - swiperWidth) / 2) / swiperWidth;
-    nextOffset = Math.max(nextOffset, 1);
-    return `${nextOffset * 100}%`;
   }
 }
