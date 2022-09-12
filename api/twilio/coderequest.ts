9dcefb99da6ff7d5b92f3f6ffd1f59fe14c419c6
missahrizona <princessssahri@gmail.com>
2022-09-11 22:18:36 -0500

    cors

--- api/twilio/coderequest.ts
+++ api/twilio/coderequest.ts
@@ -1,7 +1,27 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { twilioclient } from '../lib/_twilio';
 
-export default async function (req: VercelRequest, res: VercelResponse) {
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+const handler = async function (req: VercelRequest, res: VercelResponse) {
   try {
     let code = Math.floor(Math.random() * (999999 - 100000 + 1) + 100000);
     let textmsg = `${code} is your security code from Charmee`;
@@ -18,4 +38,6 @@ export default async function (req: VercelRequest, res: VercelResponse) {
   } catch (exception) {
     res.status(500).send(exception);
   }
-}
+};
+
+export default allowCors(handler);
