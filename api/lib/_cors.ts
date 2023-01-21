d08c5a96c1e1cca3c5d314fbb0a888b2925a4d0c
missahrizona <princessssahri@gmail.com>
2023-01-21 01:29:21 -0600

    testing api

--- api/lib/_cors.ts
+++ api/lib/_cors.ts
@@ -12,8 +12,7 @@ const cors = (fn) => async (req, res) => {
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   );
   if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
+    return res.status(200).json({ body: 'OK' });
   }
   return await fn(req, res);
 };
