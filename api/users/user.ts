8863f2f171c2771ebf3672bdf7cef44bc87f16fe
missahrizona <princessssahri@gmail.com>
2023-01-21 02:05:46 -0600

    testing api

--- api/users/user.ts
+++ api/users/user.ts
@@ -14,6 +14,8 @@ import { v4 as uuidv4 } from 'uuid';
 export default cors(async function (req: VercelRequest, res: VercelResponse) {
   if (req.method === 'OPTIONS') {
     return res.status(200).json({ body: 'OK' });
+  } else {
+    return res.status(200).json({ msg: 'success' });
   }
   if (req.body.action == 'create') {
     try {
